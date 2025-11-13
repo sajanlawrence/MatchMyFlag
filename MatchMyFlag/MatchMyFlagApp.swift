3dc367a109e85ce93329ddc7dd597f2151e3ad29
Sajan Lawrence <sajanlawrence12345@gmail.com>
2025-11-13 15:22:23 +0530

    Initial Commit

---
+++ MatchMyFlag/MatchMyFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMyFlagApp.swift
+//  MatchMyFlag
+//
+//  Created by Sajan Lawrence on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchMyFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
